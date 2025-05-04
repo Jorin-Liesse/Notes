8cc6674549591a4b7dc7cac89cefdf5a4c5fba05
Jorin Liesse <liesse.jorin@gmail.com>
2025-05-04 15:02:05 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+let data = null;
+
 function ListOnClick(element) {
-  alert("List On Click");
+  // alert("List On Click");
 
   if (element.classList.contains("done")) element.classList.remove("done");
   else element.classList.add("done");
+
+  SetItem(element.innerText, element.classList.contains("done"));
 }
+
+function SetItem(key, value) {
+  if (data === null) LoadData();
+
+  data[key] = value;
+  SaveData();
+}
+
+function GetItem(key) {
+  if (data === null) LoadData();
+
+  return data[key] || false;
+}
+
+function SaveData() {
+  localStorage.setItem(document.title, JSON.stringify(data));
+}
+
+function LoadData() {
+  data = JSON.parse(localStorage.getItem(document.title)) || {};
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  alert("DOM Loaded");
+  LoadData();
+});
