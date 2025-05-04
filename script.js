f946b2353ab5901c1f12f8e0d9de599336a14b65
Jorin Liesse <liesse.jorin@gmail.com>
2025-05-04 14:48:29 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-document.querySelectorAll('span').forEach(span => {
-    span.addEventListener('click', () => {
-      alert(`You clicked: ${span.className}`);
-      // Or run your custom logic here
-    });
-  });
+function test() {
+  alert("test")
+}
